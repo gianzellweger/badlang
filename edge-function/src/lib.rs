9ac30a86885b796f76aa291a27497e23a4e17f7d
gianzellweger <gian.zellweger@ict-scouts.ch>
2025-01-02 22:50:49 +0100

    Added biometric and security questions to 2FA :fire:

--- edge-function/src/lib.rs
+++ edge-function/src/lib.rs
@@ -1,10 +1,11 @@
-use std::time::Duration;
-
+use badlang_parser as pa;
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    tokio::time::sleep(Duration::from_millis(200));
-    console_error_panic_hook::set_once();
-    Response::ok("Hello World!")
+pub async fn main(mut req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    let mut buf: Vec<u8> = vec![];
+    match pa::execute_tokens(&pa::parse_string("'Hello World' println".to_string()).unwrap(), &mut buf, Some(std::time::Duration::from_millis(10))).await {
+        Ok(_) => Response::ok("Ok"),
+        Err(_) => Response::error("oh oh", 403),
+    }
 }
